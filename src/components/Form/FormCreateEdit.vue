7ee18bc06bd947b2e135c653dc0062be517edcb8
tunglam-it <zonakute@gmail.com>
2023-06-16 08:54:40 +0700

    doing mixins

--- src/components/Form/FormCreateEdit.vue
+++ src/components/Form/FormCreateEdit.vue
@@ -1,89 +1,101 @@
 <template>
-    <div class="col-md-8 col-sm-12 p-0 mt-4 my-3 mx-4 mx-auto">
-      <div class="card mx-2">
-        <div class="card-header">
-          <slot></slot>
-        </div>
-        <div class="card-body p-0">
-          <div class="mx-4 my-2">
-            <Form >
-              <div class="row mb-3">
-                <label class="col-sm-2 col-form-label">Tiêu đề:</label>
-                <div class="col-sm-10">
-                  <Field name='title' type="text" v-model="form.title" class="form-control" :rules="validateInput"/>
-                  <ErrorMessage name="title" class="danger text-danger"/>
-                </div>
+  <div class="col-md-8 col-sm-12 p-0 mt-4 my-3 mx-4 mx-auto">
+    <div class="card mx-2">
+      <div class="card-header">
+        <slot></slot>
+      </div>
+      <div class="card-body p-0">
+        <div class="mx-4 my-2">
+          <Form>
+            <div class="row mb-3">
+              <label class="col-sm-2 col-form-label">Loại:</label>
+              <div class="col-sm-10">
+                <Field name='type' v-model="form.type" class="form-control" as='select' :rules="validateInput">
+                  <option>-- Hãy chọn 1 hình thức --</option>
+                  <option v-for="type in types" :value="type.id" :key="type.id">{{ type.name }}</option>
+                </Field>
+                <ErrorMessage name="type" class="danger text-danger" />
               </div>
-              <div class="row mb-3">
-                <label class="col-sm-2 col-form-label">Loại:</label>
-                <div class="col-sm-10">
-                  <Field name='type' v-model="form.type" class="form-control" as='select' :rules="validateInput">
-                    <option>-- Hãy chọn 1 hình thức --</option>
-                    <option value="1">Sức khoẻ</option>
-                    <option value="2">Thai sản</option>
-                    <option value="3">Gia đình</option>
-                    <option value="4">Học tập</option>
-                  </Field>
-                  <ErrorMessage name="type"  class="danger text-danger"/>
-                </div>
+            </div>
+            <div class="row mb-3">
+              <label class="col-sm-2 col-form-label">Lý do:</label>
+              <div class="col-sm-10">
+                <Field name='reason' rows="8" v-model="form.reason" class="form-control" as='textarea'
+                  :rules="validateInput"></Field>
+                <ErrorMessage name="reason" class="danger text-danger" />
               </div>
-              <div class="row mb-3">
-                <label class="col-sm-2 col-form-label">Lý do:</label>
-                <div class="col-sm-10">
-                  <Field name='reason' rows="8" v-model="form.reason" class="form-control" as='textarea' :rules="validateInput"></Field>
-                  <ErrorMessage name="reason"  class="danger text-danger"/>
-                </div>
+            </div>
+            <div class="row mb-3">
+              <label class="col-sm-2 col-form-label">Ngày bắt đầu:</label>
+              <div class="col-sm-10">
+                <Field name='start_date' class="form-control" type="datetime-local" v-model="form.start_date"
+                  :rules="validateInput" />
+                <ErrorMessage name="start_date" class="danger text-danger" />
               </div>
-              <div class="row mb-3">
-                <label class="col-sm-2 col-form-label">Ngày bắt đầu:</label>
-                <div class="col-sm-10">
-                  <Field name='start_date' class="form-control" type="datetime-local" v-model="form.start_date" :rules="validateInput"/>
-                  <ErrorMessage name="start_date"  class="danger text-danger"/>
-                </div>
+            </div>
+            <div class="row mb-3">
+              <label class="col-sm-2 col-form-label">Ngày kết thúc:</label>
+              <div class="col-sm-10">
+                <Field name='end_date' class="form-control" type="datetime-local" v-model="form.end_date"
+                  :rules="validateInput" />
+                <ErrorMessage name="end_date" class="danger text-danger" />
               </div>
-              <div class="row mb-3">
-                <label class="col-sm-2 col-form-label">Ngày kết thúc:</label>
-                <div class="col-sm-10">
-                  <Field name='end_date' class="form-control" type="datetime-local" v-model="form.end_date" :rules="validateInput"/>
-                  <ErrorMessage name="end_date"  class="danger text-danger"/>
-                </div>
-              </div>
-              <div class="mt-3 mb-3">
-                  Thời gian xin nghỉ phép là: 
-              </div>
-              <button class="btn btn-success mb-3" type="submit" @click.prevent="handleSubmit">
-                Submit
-              </button>
-            </Form>
-          </div>
+            </div>
+            <div class="mt-3 mb-3">
+              Thời gian xin nghỉ phép là:  tiếng
+            </div>
+            <button class="btn btn-success mb-3" type="submit" @click.prevent="handleSubmit">
+              Submit
+            </button>
+          </Form>
         </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
 import { Field, Form, ErrorMessage } from 'vee-validate';
+import types from '../../leave_types/types.js';
 
 export default {
   components: { Field, Form, ErrorMessage },
-  props:{
-    form:{},
-    isInsert:null
+  props: {
+    form: {},
+    isInsert: null
   },
-  methods:{
-    validateInput(value){
+  data() {
+    return {
+      types: types
+    }
+  },
+  computed: {
+    
+  },
+  methods: {
+    /***
+     * validate for input value
+     * @param value
+     * @returns boolean
+     */
+    validateInput(value) {
       if (!value) {
         return 'This field is required';
       }
       return true
     },
-    handleSubmit(){
-      if(this.isInsert){
+
+    /***
+     * insert or update data for form
+     * @returns mixed
+     */
+    handleSubmit() {
+      if (this.isInsert) {
         this.$emit("insert", this.form);
-      }else{
+      } else {
         this.$emit("update", this.form);
       }
-    }
+    },
   }
 }
 </script>
--- src/components/Form/FormDetail.vue
+++ src/components/Form/FormDetail.vue
@@ -6,14 +6,24 @@
       </div>
       <div class="card-body">
         <div class="row">
-          <div class="col-md-6 mb-2">
-            <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Nhập tên để tìm kiếm" aria-label="Search">
+          <div class="col-md-4 mb-2">
+            <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Nhập để tìm kiếm">
             </form>
           </div>
-          <div class="col-md-6">
-            <form class="d-flex justify-content-end" role="search">
-              <input class="form-control me-2" type="date" placeholder="Nhập tên để tìm kiếm" aria-label="Search">
+          <div class="col-md-4">
+            <form class="d-flex mb-2">
+              <input class="form-control me-2" type="date" >
+            </form>
+          </div>
+          <div class="col-md-4">
+            <form class="d-flex">
+              <select class="form-control me-2" type="date" >
+                <option>-- Chọn 1 option --</option>
+                <option value="1">Chấp thuận</option>
+                <option value="2">Từ chối</option>
+                <option value="3">Đang chờ</option>
+              </select>
             </form>
           </div>
         </div>
@@ -24,7 +34,6 @@
             <tr>
               <th scope="col">ID</th>
               <th scope="col">Người tạo</th>
-              <th scope="col">Tiêu đề</th>
               <th scope="col">Loại</th>
               <th scope="col">Lý do</th>
               <th scope="col">Ngày bắt đầu</th>
@@ -35,25 +44,24 @@
             </tr>
           </thead>
           <tbody>
-            <!-- <tr>
-              <td colspan="10" v-if="leaves.length<0">Chưa có bản ghi nào</td>
-            </tr> -->
+              <tr>
+                <td colspan="10" v-if="leaves.length<0">Chưa có bản ghi nào</td>
+              </tr>
             <tr v-for="leave in leaves">
               <th scope="row">{{ leave.id }}</th>
               <th scope="row">{{ leave.employees }}</th>
-              <td>{{ leave.title }}</td>
               <td>{{ getType(leave.type) }}</td>
               <td>{{ leave.reason }}</td>
               <td>{{ leave.start_date }}</td>
               <td>{{ leave.end_date }}</td>
               <td>
-                <button type="button" class="btn btn-sm btn-fluid" style="min-width: 100px;"
+                <button type="button" class="btn btn-sm" style="min-width: 100px;"
                   :class="{ 'btn-warning': leave.status==3, 'btn-success': leave.status==1, 'btn-danger': leave.status==2 }"
                   data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">
                   <span class="text-white fw-bold">{{ checkStatus(leave.status) }}</span>
                 </button>
               </td>
-              <td></td>
+              <td>{{ leave.comment }}</td>
               <td>
                 <router-link :to="{ name: 'edit-form', params: { id: leave.id } }">
                   <button class="btn btn-sm btn-primary me-2">Sửa</button>
@@ -108,11 +116,16 @@
 import { Field, Form, ErrorMessage } from 'vee-validate';
 import types from '../../leave_types/types.js';
 import axiosClient from '../../axiosClient.js';
+import calculateTime from '../../mixins/calculateTime.js'
 
 export default {
+  mixins:[calculateTime],
   components: { Field, Form, ErrorMessage },
   props:{
       leaves: {}
+  },
+  computed:{
+
   },
   methods: {
     validateInput(value) {
